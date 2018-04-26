34338003d7b98dbf32a61fd1778899ba7be2065b
voiny <38726183+voiny@users.noreply.github.com>
2018-04-26 22:32:41 +0800

    Update list2task.sh

--- script/list2task.sh
+++ script/list2task.sh
@@ -28,7 +28,7 @@ function convert_list_to_task() {
 		fi
 		KEYS=${KEYS},\"${LINE}\"
 	done
-	echo "curl -H 'Content-Type:application/json' --insecure -X POST --data '{\"src_node\":{\"region\":\"${SRCREGION}\",\"ak\":\"${SRCAK}\",\"sk\":\"${SRCSK}\",\"object_key\":{\"path\":\"${SRCPATH_SHORT}\",\"keys\":[${KEYS}]},\"bucket\":\"${SRCBUCKETNAME}\",\"cloud_type\":\"${SRCCLOUDTYPE}\"},\"thread_num\":${THREAD_PER_TASK},\"enableKMS\":${ENABLE_KMS},\"description\":\"${DESC_PREFIX}_${ONE_LIST_FILE}\",\"dst_node\":{\"region\":\"${DSTREGION}\",\"ak\":\"${DSTAK}\",\"sk\":\"${DSTSK}\",\"object_key\":\"${DSTPATH_SHORT}\",\"bucket\":\"${DSTBUCKETNAME}\",\"cloud_type\":\"OTC\"}}' https://127.0.0.1:8099/v1/0000000000/objectstorage/task" > ${ONE_LIST_FILE}_task.sh
+	echo "curl -H 'Content-Type:application/json' --insecure -X POST --data '{\"src_node\":{\"region\":\"${SRCREGION}\",\"ak\":\"${SRCAK}\",\"sk\":\"${SRCSK}\",\"object_key\":{\"path\":\"${SRCPATH_SHORT}\",\"keys\":[${KEYS}]},\"bucket\":\"${SRCBUCKETNAME}\",\"cloud_type\":\"${SRCCLOUDTYPE}\"},\"thread_num\":${THREAD_PER_TASK},\"enableKMS\":${ENABLE_KMS},\"description\":\"${DESC_PREFIX}_${ONE_LIST_FILE}\",\"dst_node\":{\"region\":\"${DSTREGION}\",\"ak\":\"${DSTAK}\",\"sk\":\"${DSTSK}\",\"object_key\":\"${DSTPATH_SHORT}\",\"bucket\":\"${DSTBUCKETNAME}\",\"cloud_type\":\"OTC\"}}' ${SERVER_ADDRESS}" > ${ONE_LIST_FILE}_task.sh
 	chmod 550 ${ONE_LIST_FILE}_task.sh
 	if [ "${TABLE_FILE}" != "" ];then
 		echo ${ONE_LIST_FILE}	${KEYS} >> ${TABLE_FILE}
