72effc5dbfeb542f836a3e72ecc8e4f51f6b7f80
voiny <38726183+voiny@users.noreply.github.com>
2018-04-28 20:52:37 +0800

    Update conf_ossutil.sh

--- script/conf_ossutil.sh
+++ script/conf_ossutil.sh
@@ -5,7 +5,7 @@ export PATH=$PATH:/usr/bin
 
 echo [Credentials] > /root/.ossutilconfig
 echo language=EN >> /root/.ossutilconfig
-echo endpoint=${SRCREGION}.aliyuncs.com >> /root/.ossutilconfig
+echo endpoint=oss-${SRCREGION}.aliyuncs.com >> /root/.ossutilconfig
 echo accessKeyID=${SRCAK} >> /root/.ossutilconfig
 echo accessKeySecret=${SRCSK} >> /root/.ossutilconfig
 
