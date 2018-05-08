e3f0f3eb04b0714c6a9133d12f3035f5c2141465
root <root@wangfei-project-0002.novalocal>
2018-05-08 17:10:04 +0800

    new file:   basic_analyze.sh

---
+++ basic_analyze.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ORIGINAL_DIRECTORY=$(cd $(dirname $0); pwd)
+LISTPATH=$1
+
+
+cd ${LISTPATH}
+LIST=`ls`
+rm -rf ${ORIGINAL_DIRECTORY}/result.txt
+for FILE in ${LIST}; do
+	SIZE=`cat ${FILE} | awk '{sum+=$3}END{print sum}'`
+	if [ "${SIZE}" == "" ]; then
+		SIZE=0
+	fi
+	OBJECT_COUNT=`cat ${FILE} | wc -l`
+	if [ "${OBJECT_COUNT}" == "" ]; then
+		OBJECT_COUNT=0
+	fi
+	echo ${FILE} ${OBJECT_COUNT} ${SIZE}>> ${ORIGINAL_DIRECTORY}/result.txt
+done
+
+cd ${ORIGINAL_DIRECTORY}
--- batch_list4s3.sh
+++ batch_list4s3.sh
@@ -4,12 +4,13 @@ cd script
 source ./env.sh
 export PATH=$PATH:/usr/bin
 cd ..
+mkdir -p /data/tmp-result
 for i in $*; do
 	bucket_name=`echo $i | awk -F, '{print $1;}'`
 	region_id=`echo $i | awk -F, '{print $2;}'`
 	dst_file="/data/tmp-result/result-${bucket_name}"
 	rm -rf ${dst_file}
-	aws --endpoint-url=http://obs.myhwclouds.com --region=${region_id} s3 ls s3://${bucket_name}/ --recursive | tee ${dst_file} &
+	aws --endpoint-url=http://s3.cn-north-1.amazonaws.com.cn --region=${region_id} s3 ls s3://${bucket_name}/ --recursive | tee ${dst_file} &
 done
 
 wait
