d53fb9ad433afe4c8e065f31ffe4b656d45d84f5
root <root@wangfei-project-0001.novalocal>
2018-05-09 11:22:44 +0800

    new file:   batch_list4obs.sh 	modified:   batch_list4oss.sh 	modified:   batch_list4s3.sh

--- batch_list4obs.sh
+++ batch_list4obs.sh
@@ -22,7 +22,7 @@ for i in $*; do
 	region_id=`echo $i | awk -F, '{print $2;}'`
 	dst_file="${OUTPUT_PATH}/result-${bucket_name}"
 	rm -rf ${dst_file}
-	aws --endpoint-url=http://s3.cn-north-1.amazonaws.com.cn --region=${region_id} s3 ls s3://${bucket_name}/ --recursive | tee ${dst_file} &
+	aws --endpoint-url=http://obs.myhwclouds.com --region=${region_id} s3 ls s3://${bucket_name}/ --recursive | tee ${dst_file} &
 done
 
 wait
--- batch_list4oss.sh
+++ batch_list4oss.sh
@@ -4,6 +4,20 @@ cd script
 source ./env.sh
 export PATH=$PATH:/usr/bin
 cd ..
+OUTPUT_PATH=$1
+
+function usage()
+{
+	echo "[command] [OUTPUT_PATH:/data/list_result/]"
+}
+
+if [ "${OUTPUT_PATH}" == "" ];then
+	usage
+	exit
+fi
+
+mkdir -p ${OUTPUT_PATH}
+
 for i in $*; do
 	bucket_name=`echo $i | awk -F, '{print $1;}'`
 	region_id=`echo $i | awk -F, '{print $2;}'`
