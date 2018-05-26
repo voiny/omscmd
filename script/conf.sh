38c6809afe195502f1914c53e24aaaadb1d20725
joejang <joejang@163.com>
2018-05-26 13:10:02 +0800

    modified:   conf.sh

--- script/conf.sh
+++ script/conf.sh
@@ -6,17 +6,17 @@ export SRCCLOUDTYPE='Aliyun'
 export SRCTOOL='ossutil'
 #for OSS cn-beijing is OK, oss-cn-beijing is NOK
 export SRCREGION='cn-beijing'
-export SRCAK='LTAI7JYINLrQcd12'
-export SRCSK='uJyVHxEwVN4TKnowLxcyAQ82gsMlM2'
-export SRCBUCKETNAME='ys-public'
+export SRCAK=''
+export SRCSK=''
+export SRCBUCKETNAME='ys-diandiantaobao'
 export SRCPATH_SHORT='/'
 #export SRCPATH_SHORT='/xxx/'
 export SRCPATH="oss://${SRCBUCKETNAME}${SRCPATH_SHORT}"
 export DSTREGION='cn-north-1'
-export DSTAK='UUAAQCVCB2PU1YPTFFRG'
-export DSTSK='JXKsagokV2XxJDn21e5XdqVrqjjmE4die50ThoOt'
+export DSTAK=''
+export DSTSK=''
 #DSTBUCKETNAME should not contain 's3://'
-export DSTBUCKETNAME='ys-public'
+export DSTBUCKETNAME='ys-diandiantaobao'
 #DSTPATH should not contain 's3://'
 #export DSTPATH_SHORT='xxx/' (set '/' for root for OMS, set'' for root for migrate_list* scripts)
 export DSTPATH_SHORT=''
@@ -26,7 +26,7 @@ export NAME='omscmd'
 export ENABLE_KMS='false'
 export DESC_PREFIX='Migration_Project'
 export WORKSPACE=/data/tmp/${NAME}
-export OBJ_CNT_IN_SPLIT=300
+export OBJ_CNT_IN_SPLIT=5
 export THREAD_PER_TASK=50
 export DB_PWD='xxx'
 
