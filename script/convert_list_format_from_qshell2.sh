4ba7d489d51e153fec4a34ab1f3c44a319411b1a
joejang <joejang@163.com>
2018-09-07 15:37:19 +0800

    new file:   convert_list_format_from_qshell2.sh

---
+++ script/convert_list_format_from_qshell2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#output: timestamp key size hash
+
+if [ "${ORIGINAL_DIRECTORY}" != "" ]; then
+	cd ${ORIGINAL_DIRECTORY}
+fi
+source ./conf.sh
+
+LINE=$1
+REMOVE_STRING=$2
+cd ${WORKSPACE}/lists
+cat ${LINE} | awk -F'\t' '{t1=substr($4,0,13);printf t1; for (i=1;i<NF-2;i++) printf(" %s", $i); printf "\n" }' > ${WORKSPACE}/converted_lists/${LINE}_converted_format
