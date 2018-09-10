d046cc5e8c93533a28a8c469959f110ba40bbdd1
joejang <joejang@163.com>
2018-09-10 17:01:27 +0800

    modified:   basic_analyze.sh

--- basic_analyze.sh
+++ basic_analyze.sh
@@ -2,32 +2,49 @@
 
 ORIGINAL_DIRECTORY=$(cd $(dirname $0); pwd)
 LISTPATH=$1
-OUTPUT_FILE=$2
+FORMAT=$2
+OUTPUT_FILE=$3
 
 function usage() {
-	echo "[command] [LISTPATH] [OUTPUT_FILE]"
+	echo "[command] [LISTPATH] [FORMAT(osf/olf)] [OUTPUT_FILE]"
 }
 
-if [[ "${LISTPATH}" == "" || "${OUTPUT_FILE}" == "" ]]; then
+if [[ "${LISTPATH}" == "" || "${OUTPUT_FILE}" == "" || "${FORMAT}" == "" ]]; then
 	usage
 	exit
 fi
 
-
 cd ${LISTPATH}
 LIST=`ls ${LISTPATH}`
 rm -rf ${OUTPUT_FILE}
-for FILE in ${LIST}; do
-	SIZE=`cat ${FILE} | awk '{sum+=$NF}END{print sum}'`
-	if [ "${SIZE}" == "" ]; then
-		SIZE=0
-	fi
-	OBJECT_COUNT=`cat ${FILE} | wc -l`
-	if [ "${OBJECT_COUNT}" == "" ]; then
-		OBJECT_COUNT=0
-	fi
-	echo ${FILE} ${OBJECT_COUNT} ${SIZE}>> ${OUTPUT_FILE}
-done
+if [ "${FORMAT}" == "osf" ]; then
+	for FILE in ${LIST}; do
+		SIZE=`cat ${FILE} | awk '{sum+=$NF}END{print sum}'`
+		if [ "${SIZE}" == "" ]; then
+			SIZE=0
+		fi
+		OBJECT_COUNT=`cat ${FILE} | wc -l`
+		if [ "${OBJECT_COUNT}" == "" ]; then
+			OBJECT_COUNT=0
+		fi
+		echo ${FILE} ${OBJECT_COUNT} ${SIZE}>> ${OUTPUT_FILE}
+	done
+elif [ "${FORMAT}" == "olf" ]; then
+	for FILE in ${LIST}; do
+		SIZE=`cat ${FILE} | awk '{sum+=$3}END{print sum}'`
+		if [ "${SIZE}" == "" ]; then
+			SIZE=0
+		fi
+		OBJECT_COUNT=`cat ${FILE} | wc -l`
+		if [ "${OBJECT_COUNT}" == "" ]; then
+			OBJECT_COUNT=0
+		fi
+		echo ${FILE} ${OBJECT_COUNT} ${SIZE}>> ${OUTPUT_FILE}
+	done
+else
+	usage
+	exit
+fi
 
 cat ${OUTPUT_FILE}
 
